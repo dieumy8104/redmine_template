4c829f69d05b2bcf3c9394ddb4ee2e6b9a72de0b
dieu my <mynguyen.vo0974@gmail.com>
2023-08-14 11:54:16 +0700

    feature #11 create list project page

--- js/custom.js
+++ js/custom.js
@@ -36,12 +36,12 @@ window.addEventListener('resize', function () {
         $('.section-heading-search-select-mobile-open').css('display', 'block');
         $('.section-heading-search-select-mobile-close').css('display', 'none');
         $('.section-heading-search-group-select-project').removeClass("search-navbar");
-     }else {
+    } else {
         $('.section-heading-project-select').css('display', 'none');
         $('.section-heading-search-select-open').css('display', 'block');
         $('.section-heading-search-select-close').css('display', 'none');
         $('.section-heading-search-group-select-project').removeClass("search-navbar");
-     }
+    }
 })
 
 // Section Heading Search
@@ -62,6 +62,131 @@ $('.section-heading-search-group-select-project').click(function (e) {
 });
 
 // Section Content Checkbox
-$("#checkbox-all").click(function(){
+$("#checkbox-all").click(function () {
     $(".issue-checkbox").prop('checked', $(this).prop('checked'));
+});
+
+// Section Content Sidebar
+$('.section-heading-search-menu-button').click(function (e) {
+    e.stopPropagation();
+    $('.section-content-sidebar').toggle();
+});
+
+$('body, html').click(function (e) {
+    let sidebarContent = $(".section-content-sidebar");
+    if (!sidebarContent.is(e.target) && sidebarContent.has(e.target).length === 0) {
+        sidebarContent.css('display', 'none');
+    }
+})
+
+window.addEventListener('resize', function () {
+    if ($(window).width() >= 768) {
+        $(".section-content-sidebar").css('display', 'none');
+    }
+})
+
+// Section Content Filters
+$('.section-content-project-filters-list-rotate').click(function (e) {
+    if ($(window).width() >= 768) {
+        if ($('.section-content-project-filters-select').css('display') === 'none') {
+            $('.section-content-project-filters-list-rotate-icon ').addClass("filters-rotate-on")
+            $('section-content-project-filters-list-rotate-icon').removeClass("filters-rotate-off")
+            $('.section-content-project-filters').removeClass('filters-spacing-off')
+            $('.section-content-project-filters').addClass('filters-spacing-on')
+        } else {
+            $('.section-content-project-filters-list-rotate-icon').addClass("filters-rotate-off")
+            $('.section-content-project-filters-list-rotate-icon ').removeClass("filters-rotate-on")
+
+            $('.section-content-project-filters').addClass('filters-spacing-off')
+            $('.section-content-project-filters').removeClass('filters-spacing-on')
+        }
+        $('.section-content-project-filters-select').toggle()
+    } else {
+        if ($('.section-content-project-filters-select').css('display') === 'none') {
+            $('.section-content-project-filters-list-rotate-icon ').addClass("filters-rotate-on")
+            $('section-content-project-filters-list-rotate-icon').removeClass("filters-rotate-off");
+            $('.section-content-project-filters-select').addClass("filters-display")
+            $('.section-content-project-filters').addClass('filters-spacing-mobile-on')
+            $('.section-content-project-filters').removeClass('filters-spacing-mobile-off')
+
+        } else {
+            $('.section-content-project-filters-list-rotate-icon').addClass("filters-rotate-off")
+            $('.section-content-project-filters-list-rotate-icon').removeClass("filters-rotate-on")
+            $('.section-content-project-filters-select').removeClass("filters-display")
+            $('.section-content-project-filters').removeClass('filters-spacing-mobile-on')
+            $('.section-content-project-filters').addClass('filters-spacing-mobile-off')
+        }
+    }
+});
+
+if ($('.filters-select-group').css('display') === "none") {
+    $("#filtersStatus").prop("checked", false);
+} else {
+    $("#filtersStatus").prop("checked", true);
+}
+
+$('#filtersStatus').click(function (e) {
+    e.stopPropagation();
+    $('.filters-select-group').toggle()
+});
+
+// Section Content Options
+$('.section-content-project-options-list-rotate').click(function (e) {
+    e.stopPropagation();
+    $('.section-content-project-options-toggle').toggle();
+
+    //Options Rotate
+    if ($('.section-content-project-options-toggle').css('display') === 'none') {
+        $('.section-content-project-options-list-rotate i').addClass("options-rotate-off")
+        $('.section-content-project-options-list-rotate i').removeClass("options-rotate-on")
+    } else {
+        $('.section-content-project-options-list-rotate i').addClass("options-rotate-on")
+        $('section-content-project-options-list-rotate i').removeClass("options-rotate-off")
+    }
+
+    //Options Checked
+    if ($('.section-content-project-options-select').css('display') === 'none') {
+        $("#optionsList").prop("checked", false);
+        $("#optionsBoard").prop("checked", true);
+    } else {
+        $("#optionsList").prop("checked", true);
+    }
+});
+
+$('#optionsList').click(function (e) {
+    e.stopPropagation();
+    $('.section-content-project-options-select').css('display', 'flex')
+    $('.section-content-project-options-select-list').css('display', 'flex')
+});
+
+$('#optionsBoard').click(function (e) {
+    e.stopPropagation();
+    $('.section-content-project-options-select').css('display', 'none')
+    $('.section-content-project-options-select-list').css('display', 'none')
+});
+
+// Options Select
+var select1 = document.getElementById('optionsSelect1');
+var select2 = document.getElementById('optionsSelect2');
+var moveRightButton = document.getElementById('optionsRightButton');
+var moveLeftButton = document.getElementById('optionsLeftButton');
+
+moveRightButton.addEventListener('click', () => {
+    for (var i = 0; i < select1.options.length; i++) {
+        if (select1.options[i].selected) {
+            select1.options[i].selected = false;
+            select2.appendChild(select1.options[i]);
+            i--;
+        }
+    }
+});
+
+moveLeftButton.addEventListener('click', () => {
+    for (var i = 0; i < select2.options.length; i++) {
+        if (select2.options[i].selected) {
+            select2.options[i].selected = false;
+            select1.appendChild(select2.options[i]);
+            i--;
+        }
+    }
 });
